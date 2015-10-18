1ef59473700e85ce0074d6b6243cfbdc3ba32541
John Hancock <jhancock1975@gmail.com>
2015-10-17 22:27:00 -0400

    committing before major overhaul

--- cluster-ctl.sh
+++ cluster-ctl.sh
@@ -13,8 +13,33 @@
 # machine installed on said host machine
 # hostname and guest-name should be separated by whitespace
 # lines can begin with whitespace
+function startvm {
+  host=$1;
+  guest=$2;
+  ssh $host "VBoxHeadless --startvm $guest" &
+}
+
+contains() {
+  for word in $1; do
+    [[ $word = $2 ]] && return 0
+  done
+  return 1
+}
+
+usage_msg='usage: cluster-ctl [-f host_file] start|stop
+second line';
+
+function exit_valid_cmds_msg {
+  echo $usage_msg;
+  exit 1;
+}
+
+cmd=$2;
+cmds='start stop';
+contains cmds cmd && continue || exit_valid_cmds_msg;
+
 while read host guest
 do
   [[ "$host" =~ ^#.*$ ]] && continue
-  ssh $host "VBoxHeadless --startvm $guest" &
+  startvm $host $guest
 done < $1
