20a01044620885097506d0a9470da13046dd645f
John Hancock <jhancock1975@gmail.com>
2016-10-29 11:18:05 +0000

    update from local, working copy of script

--- util/xmlpretty.sh
+++ util/xmlpretty.sh
@@ -5,6 +5,8 @@ if [ "$#" -ne 1 ] ; then
   echo "usage $0 filename"
   echo "Please note: this utility will overwrite the existing file"
   echo "with its pretty-printed equivalent."
+  exit 0
+fi
 fileName=$1
 xmllint --format $1 > $1.tmp
 mv $1.tmp $1
